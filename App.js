78b98718165b11cc529203e9c330b1ecde82fa9a
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2017-08-12 08:04:56 +0200

    setup gradients.json & Flatlist & LinearGradient

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import allGradients from './gradients.json';
+import sampleSize from 'lodash/sampleSize';
+import { LinearGradient } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    gradients: sampleSize(allGradients, 10),
+  };
+
   render() {
+    const { gradients } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          data={gradients}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item }) =>
+            <LinearGradient
+              colors={item.colors}
+              style={[styles.gradientWrapper]}
+            >
+              <Text style={styles.text}>
+                {item.name}
+              </Text>
+            </LinearGradient>}
+        />
       </View>
     );
   }
@@ -17,7 +35,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    backgroundColor: 'transparent',
+  },
+  gradientWrapper: {
+    height: 30,
   },
 });
